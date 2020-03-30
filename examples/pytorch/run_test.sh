ecaa338a678663fc26e0b6bf8a3593f6ae607e24
Zheng Zhong <tonitick@hotmail.com>
2020-03-30 11:24:18 +0800

    add some profiling

---
+++ examples/pytorch/run_test.sh
@@ -0,0 +1,10 @@
+for model in resnet50
+do
+	export CUDA_VISIBLE_DEVICES=0,1
+	mpirun --allow-run-as-root -np 2 -H localhost:2 -bind-to none -map-by slot -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib python pytorch_horovod_benchmark.py --num-iters 200 --model=$model > ./${model}_1.txt &
+	export CUDA_VISIBLE_DEVICES=2,3
+
+	mpirun --allow-run-as-root -np 2 -H localhost:2 -bind-to none -map-by slot -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib python pytorch_horovod_benchmark.py --num-iters 200 --model=$model > ./${model}_2.txt
+
+	wait
+done
